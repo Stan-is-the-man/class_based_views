e59f069e2536fcf0122ec9fbf88592cc314d992b
Stanislav Yanakiev <stan5050@gmail.com>
2022-11-06 22:05:24 +0200

    Add on

--- class_based_views/web/urls.py
+++ class_based_views/web/urls.py
@@ -1,3 +1,14 @@
+from django.urls import path
+
+from class_based_views.web.views import IndexView, EmployeeListView, EmployeeDetailsView, CreateEmployeeView, \
+    UpdateEmployeeView, DeleteEmployeeView
+
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('employees/', EmployeeListView.as_view(), name='all_employees'),
+    path('details/<int:pk>/', EmployeeDetailsView.as_view(), name='details'),
+    path('create/', CreateEmployeeView.as_view(), name='create'),
+    path('update/<int:pk>/', UpdateEmployeeView.as_view(), name='update'),
+    path('delete/<int:pk>/', DeleteEmployeeView.as_view(), name='delete'),
 
 ]
--- class_based_views/web/views.py
+++ class_based_views/web/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from class_based_views.web.models import Employee
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class EmployeeListView(ListView):
+    model = Employee
+    template_name = 'employees.html'
+    context_object_name = 'employees'
+
+
+class EmployeeDetailsView(DetailView):
+    model = Employee
+    template_name = 'details.html'
+
+    # context_object_name = 'employees'
+
+
+class CreateEmployeeView(CreateView):
+    model = Employee
+    template_name = 'create_employee.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('details', kwargs={
+            'pk': self.object.pk,
+        })
+    # or just to employee page:
+    #     return reverse_lazy('all_employees')
+
+
+class UpdateEmployeeView(UpdateView):
+    model = Employee
+    template_name = 'update_employee.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('details', kwargs={
+            'pk': self.object.pk,
+        })
+
+    def dispatch(self, request, *args, **kwargs):
+        # if the employee to update is the same as the user logged in => continue
+        return super().dispatch(request, *args, **kwargs)
+
+
+class DeleteEmployeeView(DeleteView):
+    model = Employee
+    template_name = 'delete_employee.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('all_employees')
 
